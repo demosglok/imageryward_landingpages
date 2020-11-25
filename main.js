3c5340027953205e19e604a335c87b84e3364a0c
Dmytro Selin <dmytroselin@fb.com>
2020-11-25 20:40:02 +0000

    js form sending and some text changes

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-document.onload = function() {
-    console.log('document onload');
-}
-document.addEventListener('load', () => console.log('aaa'));
 
+window.addEventListener('load', () => {
+    const submit_button = document.getElementById('send_form_button');
+    submit_button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const data = {
+            name: document.getElementById('form_name')?.value,
+            contact: document.getElementById('form_contact')?.value,
+            message: document.getElementById('form_message')?.value
+        }
+        fetch('/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        }).then(resp => resp.json())
+        .then(resp_data => {
+            document.getElementById('form_name').value = '';
+            document.getElementById('form_contact').value = '';
+            document.getElementById('form_message').value = '';
+            document.getElementById('form_header').innerHTML = 'Thank you, your message saved. Feel free to send more';
+            console.log('got response', resp_data);
+        }).catch(err => {
+            console.log('got error', err.message);
+            document.getElementById('form_error').innerHTML = `Error happened, try again later`;
+        })
+    });
+});
